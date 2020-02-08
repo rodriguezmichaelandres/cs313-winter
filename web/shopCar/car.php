e1afe8ea5b339b2427390960e71dd1eb5c828d26
andresrodrock <andresrodrock@gmail.com>
2020-02-08 16:22:25 -0500

    change php and html

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -7,10 +7,7 @@ echo '<link rel="stylesheet" href="car.css" type="text/css">';
 if(isset($_POST["validate"]))
 { 
     if(isset($_POST["book_0"])){
-        echo "<style>margin-left: 5rem; 
-		margin-right: 5rem;
-		</style>"."
-		The book thief" . "<br>" . "<img src='https://shereads.com/wp-content/uploads/2019/05/91pZEozf9qL-1.jpg' class='photosBooks'/>";
+        echo "<b>" . "The book thief" . "<br>" . "<img src='https://shereads.com/wp-content/uploads/2019/05/91pZEozf9qL-1.jpg' class='photosBooks'/></b>";
 		
 		//echo 'book0';
 	}
