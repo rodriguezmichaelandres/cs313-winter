c8173206d883752cb44bbf20d869b1fa698bf7da
andresrodrock <andresrodrock@gmail.com>
2020-02-08 16:16:06 -0500

    change php and html

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -5,10 +5,10 @@
 
 
 if(isset($_POST["validate"]))
-{
-	echo "inside 1"; 
+{ 
     if(isset($_POST["book_0"])){
-        echo "The book thief" . "<br>" . "hol";
+        echo "The book thief" . "<br>" . "<img src='https://shereads.com/wp-content/uploads/2019/05/91pZEozf9qL-1.jpg' class='photosBooks'/>";
+		
 		//echo 'book0';
 	}
 }
