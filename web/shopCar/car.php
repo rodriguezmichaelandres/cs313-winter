352232dca000acf7c145a90ce99b3648aed64225
andresrodrock <andresrodrock@gmail.com>
2020-02-08 15:56:39 -0500

    change php and html

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -2,41 +2,14 @@
 //session_start();
 
 //print_r($_SESSION);
-echo "before if";
+echo "before";
 
-$arr_use = $_POST["book"];
-
-for($i=0; $i<count($arr_use); $i++)
+if(isset($_POST["validate"]))
 {
-	if($arr_use[$i] != NULL){
-		echo $arr_use[$i];
-	}
-
-/*if(!(empty($_POST['book']))){
-	echo "<br>";
-	foreach($_POST['book'] as $book){		
-		echo $book."<br>";	
-		echo "close if statement";
-	}
-	
+	if(isset($_POST["book_0"]))
+		echo $_POST["book_0"]'
 }
-
-$active = filter_input(INPUT_POST, 'product', FILTER_VALIDATE_BOOLEAN);
-
-if (!isset($POST['product']))
-	$active = 0;
-echo $active;*/
-/*$active = [];
-
-foreach ($_POST['checkboxes'] as $option){
-	if($option){
-		$active[$option] = 1; 
-	} else {
-		$active[$option] = 0;
-	}
-}*/
-
-echo "hola"; 
+echo "after"; 
 
 //session_destroy();
 ?>
