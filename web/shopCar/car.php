6c385415dcbb0839c1ae7e6f5f0e400433f2a91e
andresrodrock <andresrodrock@gmail.com>
2020-02-08 16:39:07 -0500

    change php, css and html

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -8,22 +8,22 @@ echo '<link rel="stylesheet" href="car.css" type="text/css">';
 if(isset($_POST["validate"]))
 { 
     if(isset($_POST["book_0"])){
-        echo "<b>" . "The book thief" . "<br>" . "<img src='https://shereads.com/wp-content/uploads/2019/05/91pZEozf9qL-1.jpg'/></b>";
+        echo "<b>" . "The book thief" . "<br>" . "<img src='https://shereads.com/wp-content/uploads/2019/05/91pZEozf9qL-1.jpg' class='photosBooksPhp'/></b>";
 	}
 	if(isset($_POST["book_1"])){
-        echo "<b>" . "The last year of the war" . "<br>" . "<img src='https://images1.penguinrandomhouse.com/cover/9780451492159' /></b>";
+        echo "<b>" . "The last year of the war" . "<br>" . "<img src='https://images1.penguinrandomhouse.com/cover/9780451492159' class='photosBooksPhp'/></b>";
 	}
 	if(isset($_POST["book_2"])){
-        echo "<b>" . "Life after life" . "<br>" . "<img src='https://somaggiesays.files.wordpress.com/2019/06/life-after-life.png?w=300&h=464'/></b>";
+        echo "<b>" . "Life after life" . "<br>" . "<img src='https://somaggiesays.files.wordpress.com/2019/06/life-after-life.png?w=300&h=464' class='photosBooksPhp'/></b>";
 	}
 	if(isset($_POST["book_3"])){
-        echo "<b>" . "The thousand bomber raids" . "<br>" . "<img src='https://cdn.shopify.com/s/files/1/1819/1457/products/Bomber_Command_The_Thousand_Bomber_Raids_800x.jpg?v=1514533332'/></b>";
+        echo "<b>" . "The thousand bomber raids" . "<br>" . "<img src='https://cdn.shopify.com/s/files/1/1819/1457/products/Bomber_Command_The_Thousand_Bomber_Raids_800x.jpg?v=1514533332' class='photosBooksPhp'/></b>";
 	}
 	if(isset($_POST["book_4"])){
-        echo "<b>" . "Girl in the blue coat" . "<br>" . "<img src='https://img.washingtonpost.com/rf/image_480w/2010-2019/WashingtonPost/2016/03/31/BookWorld/Images/c5887bd84f019a649d2a0d2ed64d55d4.jpg?uuid=F9-dcvdlEeWjzvBrW6IfMw'/></b>";
+        echo "<b>" . "Girl in the blue coat" . "<br>" . "<img src='https://img.washingtonpost.com/rf/image_480w/2010-2019/WashingtonPost/2016/03/31/BookWorld/Images/c5887bd84f019a649d2a0d2ed64d55d4.jpg?uuid=F9-dcvdlEeWjzvBrW6IfMw' class='photosBooksPhp'/></b>";
 	}
 	if(isset($_POST["book_5"])){
-        echo "<b>" . "The fire by night" . "<br>" . "<img src='https://images-na.ssl-images-amazon.com/images/I/51rJwAZgS4L._SX331_BO1,204,203,200_.jpg'/></b>";
+        echo "<b>" . "The fire by night" . "<br>" . "<img src='https://images-na.ssl-images-amazon.com/images/I/51rJwAZgS4L._SX331_BO1,204,203,200_.jpg' class='photosBooksPhp'/></b>";
 	}
 	
 }
