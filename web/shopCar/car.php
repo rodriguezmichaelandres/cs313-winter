05de96447ecc15db5275c208937813eabaa573dc
andresrodrock <andresrodrock@gmail.com>
2020-02-08 16:09:06 -0500

    change php and html

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -7,7 +7,7 @@ echo "before";
 if(isset($_POST["validate"]))
 {
 	echo "inside 1"; 
-    if(isset($_POST["book0"])){
+    if(isset($_POST["book_0"])){
         echo $_POST["book0"];
 		echo 'book0';
 	}
