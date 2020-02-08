09ee50ea4cc335819fec8fa1223b3bab1f618a7f
andresrodrock <andresrodrock@gmail.com>
2020-02-08 12:59:27 -0500

    modified php

--- web/shopCar/car.php
+++ web/shopCar/car.php
@@ -2,6 +2,21 @@
 session_start();
 
 print_r($_SESSION);
+/*
+$active = filter_input(INPUT_POST, 'product', FILTER_VALIDATE_BOOLEAN);
+
+if (!isset($POST['product']))
+	$active = 0;*/
+$active = [];
+
+foreach ($_POST['checkboxes'] as $option){
+	if($option){
+		$active[$option] = 1; 
+	} else {
+		$active[$option] = 0;
+	}
+}
+
 
 session_destroy();
 ?>
